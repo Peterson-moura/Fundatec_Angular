50e5f157af2232ad489e652fa66018bd36cd5517
Peterson-moura <peterson.moura@outlook.com.br>
2022-05-22 20:40:39 -0300

    _bancoMongoDB_get

---
+++ Fundatec_Mogodb/index.js
@@ -0,0 +1,138 @@
+// config inicial
+var express = require('express')
+var cors = require('cors')
+var app = express()
+var corsOptions = {
+  origin: 'http://localhost:3000/calender',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+
+app.use(cors()) //Essa linha aqui
+
+
+
+// depois do db
+const mongoose = require('mongoose')
+
+const Calender = require('./models/Calender')
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  }),
+)
+
+app.use(express.json())
+
+// rotas
+app.post('/calender', async (req, res) => {
+  const {Curso, Turma,Turno,Disciplina,Professor,Data} = req.body
+
+  const calender = {
+    Curso,
+    Turma,
+    Turno,
+    Disciplina,
+    Professor,
+    Data
+  }
+
+  try {
+    await Calender.create(calender)
+
+    res.status(201).json({ message: 'Agendado com Sucesso' })
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+app.get('/calender', async (req, res) => {
+  try {
+    const calender = await Calender.find()
+
+    res.status(200).json(calender)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+app.get('/calender/:id', async (req, res) => {
+  const id = req.params.id
+
+  try {
+    const calender = await Calender.findOne({ _id: id })
+
+    if (!calender) {
+      res.status(422).json({ message: 'Agenda não encontrada' })
+      return
+    }
+
+    res.status(200).json(calender)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+app.patch('/calender/:id', async (req, res) => {
+  const id = req.params.id
+
+  const {curso,turma, turno,disciplina,professor,data } = req.body
+
+  const calender = {
+    curso,
+    turma,
+    turno,
+    disciplina,
+    professor,
+    data
+  }
+
+  try {
+    const updatedCalender = await Calender.updateOne({ _id: id }, calender)
+
+    if (updatedCalender.matchedCount === 0) {
+      res.status(422).json({ message: 'Usuário não encontrado!' })
+      return
+    }
+
+    res.status(200).json(person)
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+app.delete('/calender/:id', async (req, res) => {
+  const id = req.params.id
+
+  const calender = await Calender.findOne({ _id: id })
+
+  if (!calender) {
+    res.status(422).json({ message: 'Usuário não encontrado!' })
+    return
+  }
+
+  try {
+    await Calender.deleteOne({ _id: id })
+
+    res.status(200).json({ message: 'Usuário removido com sucesso!' })
+  } catch (error) {
+    res.status(500).json({ erro: error })
+  }
+})
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Pagina inicial' })
+})
+
+mongoose
+  .connect(
+      "mongodb+srv://Peterson:fundatec123@clusterapi.rakfd.mongodb.net/?retryWrites=true&w=majority",
+  )
+  .then(() => {
+    console.log('Conectou ao banco!')
+    
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
